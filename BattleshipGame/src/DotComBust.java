3352f6d9e3358ac0fe7752468302bbbf7fdc15c8
kajan M <kajan M>
2017-04-08 08:58:19 +0530

    extra methods to get out of main

--- BattleshipGame/src/DotComBust.java
+++ BattleshipGame/src/DotComBust.java
@@ -5,37 +5,92 @@ import java.util.ArrayList;
  */
 
 public class DotComBust {
-    public static void main(String[] args) {
-        int noOfGuesses = 0;
-        GameHelper input = new GameHelper();
+    private ArrayList<DotCom> dotComList = new ArrayList();
+    private GameHelper gameHelper = new GameHelper();
+    private int noOfGuesses = 0;
 
-        DotCom dotcom = new DotCom();
+    public void setUpGame() {
+        //initialize the DotCom objects with names and locations
+        //display brief instruction to the user
+        DotCom dotcom1 = new DotCom();
+        DotCom dotcom2 = new DotCom();
+        DotCom dotcom3 = new DotCom();
 
-        int startIndex = (int)(Math.random()*5);
+        dotcom1.setName("java.com");
+        dotcom2.setName("is.com");
+        dotcom3.setName("great.com");
 
-        ArrayList locationCells = new ArrayList<String>();
-        locationCells.add(Integer.toString(startIndex));
-        locationCells.add(Integer.toString(startIndex + 1));
-        locationCells.add(Integer.toString(startIndex + 2));
+        dotComList.add(dotcom1);
+        dotComList.add(dotcom2);
+        dotComList.add(dotcom3);
 
+        System.out.println("Your goal is to sink three dot.coms.");
+        System.out.println("java.com, is.com, great.com");
+        System.out.println("Try to sink them all in the fewest number of guesses");
 
+        for (DotCom dotCom : dotComList){
+            ArrayList<String> newLocation = gameHelper.placeDotCom(3);
+            dotCom.setLocationCells(newLocation);
+        }
 
-        dotcom.setLocationCells(locationCells);
+    }
 
-        boolean isAlive = true;
+    public void startPlaying() {
+        //asks the player for guesses
+        //calls the checkUserGuess() method
+        String userGuess;
 
-        while (isAlive){
-            String guess = input.getUserInput("enter a number");
-            String result = dotcom.checkYourSelf(guess);
+        while (!dotComList.isEmpty()){
+            userGuess = gameHelper.getUserInput("Enter a guess");
+            checkUserGuess(userGuess);
+        }
 
-            noOfGuesses++;
+        finishGame();
+    }
+
+    public void checkUserGuess(String userGuess) {
+        //loops through all remaining DotCom objects
+        //calls each DotCom object's checkYourSelf() mehtod
+        noOfGuesses++;
+        String result = "miss";
+
+        for (DotCom dotCom : dotComList){
+            result = dotCom.checkYourSelf(userGuess);
+
+            if(result.equals("hit")){
+                break;
+            }
 
             if(result.equals("kill")){
-                isAlive = false;
-                System.out.println("you took " + noOfGuesses + " guesses");
+                dotComList.remove(dotCom);
+                break;
             }
 
         }
 
+        System.out.printf(result);
+
+
+    }
+
+    public void finishGame() {
+        //prints a message about the user's performance based on how many guesses took
+        System.out.println("All Dot Coms are dead!");
+
+        if(noOfGuesses<=18){
+            System.out.println("It only took you " + noOfGuesses + " gusses");
+            System.out.println("You got our before your options sank");
+        }else{
+            System.out.println("Took long enough. "+ noOfGuesses + " guesses.");
+            System.out.println("Fish are dancing with your options");
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
     }
 }
